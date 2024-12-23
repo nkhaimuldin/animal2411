3d8c6de5d2c2cba2f33503fef8cb0867f399ff0b
Nursltan Khaimuldin <n.khaimuldin@astanait.edu.kz>
2024-12-23 08:36:08 +0500

    Added Bird class with constructor and its subclass

---
+++ src/Bird.java
@@ -0,0 +1,12 @@
+public class Bird extends Animal {
+
+    public Bird() {
+        super();
+    }
+
+    public Bird(String name, int age, boolean gender, int height, int weight) {
+        super(name, age, gender, height, weight);
+    }
+
+
+}
